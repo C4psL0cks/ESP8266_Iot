ace33ea328dfd05e2effada7931b10e92e9d9155
C4psL0cks <fenixalone@hotmail.com>
2018-06-21 17:45:22 +0700

    up

---
+++ NodemcuLCD.ino
@@ -0,0 +1,15 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin();
+}
+void loop() {
+
+  //// time //////
+  lcd.setCursor(0, 2);
+  lcd.print("Nodemcu test LED");
+
+}
