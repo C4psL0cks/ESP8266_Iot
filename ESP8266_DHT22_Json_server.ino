24987f654221cd2056ac296a47958c2d309b7167
Esc <fenixalone@hotmail.com>
2018-08-15 11:45:04 +0700

    Update ESP8266_DHT22_Json_server.ino

--- ESP8266_DHT22_Json_server.ino
+++ ESP8266_DHT22_Json_server.ino
@@ -2,82 +2,74 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
+#include "ThingSpeak.h"
+#include <TridentTD_LineNotify.h>
 
-#define DHTPIN 4
+#define DHTPIN D4
 #define DHTTYPE DHT22
-
 DHT dht(DHTPIN, DHTTYPE);
 
+int timeSinceLastRead = 0;
 const char* WIFI_SSID = "6021607";
 const char* WIFI_PASS = "17401449";
-int timeSinceLastRead = 0;
+unsigned long myChannelNumber = 495104;
+const char * myWriteAPIKey = "13GJ1GJ1VIX8KXQQ";
+#define LINE_TOKEN "lTdXXOfUCH00vgQWgjRNdt83bVptSk0yQysFezcVIvp"
+WiFiClient client;
 
-WiFiClientSecure wifiClient;
+void setup() {
 
-void connect() {
-  Serial.println("Starting...");
+  Serial.begin(115200);
+  Serial.println("Device Started");
+  Serial.println("-------------------------------------");
+  Serial.println("Running DHT!");
+  Serial.println("-------------------------------------");
+  // Connect to Wifi.
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(250);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
     Serial.print(".");
   }
+  Serial.println();
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-}
-void setup() {
-  Serial.begin(9600);
-  connect();
-  Serial.println("Running DHT!");
+  Serial.println();
+  LINE.setToken(LINE_TOKEN);
+  Serial.println(LINE.getVersion());
+  ThingSpeak.begin(client);
+  dht.begin();
 }
 void loop() {
 
-  bool toReconnect = false;
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Disconnected from WiFi");
-    toReconnect = true;
-  }
-  if (toReconnect) {
-    connect();
-  }
-  // Report every 2 seconds.
   if (timeSinceLastRead > 2000) {
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t = dht.readTemperature();
-    // Read temperature as Fahrenheit (isFahrenheit = true)
-    float f = dht.readTemperature(true);
+    float Humidity = dht.readHumidity();
+    float Temperature = dht.readTemperature();
+    float Fahrenheit = dht.readTemperature(true);
 
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t) || isnan(f)) {
-      Serial.println("Failed to read from DHT sensor!");
+    if (isnan(Humidity) || isnan(Temperature) || isnan(Fahrenheit)) {
       timeSinceLastRead = 0;
       return;
     }
+    float hif = dht.computeHeatIndex(Fahrenheit, Humidity);
+    float hic = dht.computeHeatIndex(Temperature, Humidity, false);
+    Serial.print("Humidity: "); Serial.print(Humidity); Serial.print(" %\t");
+    Serial.print("Temperature: "); Serial.print(Temperature); Serial.print(" *C ");
+    Serial.print(Fahrenheit); Serial.print(" *F\t");
+    Serial.print("Heat index: "); Serial.print(hic); Serial.print(" *C ");
+    Serial.print(hif); Serial.println(" *F");
 
-    // Compute heat index in Fahrenheit (the default)
-    float hif = dht.computeHeatIndex(f, h);
-    // Compute heat index in Celsius (isFahreheit = false)
-    float hic = dht.computeHeatIndex(t, h, false);
+    ThingSpeak.writeField(myChannelNumber, 1, Temperature, myWriteAPIKey);
+    ThingSpeak.writeField(myChannelNumber, 2, Humidity, myWriteAPIKey);
 
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.print(" *C ");
-    Serial.print(f);
-    Serial.print(" *F\t");
-    Serial.print("Heat index: ");
-    Serial.print(hic);
-    Serial.print(" *C ");
-    Serial.print(hif);
-    Serial.println(" *F");
-    report(h, t, f, hic, hif); //send to function
-
+    if (Temperature > 35) {
+      LINE.notify("Alert now temp so hot is" + String(Temperature));
+    }
+    report(Humidity , Temperature, Fahrenheit, hic, hif);
     timeSinceLastRead = 0;
   }
   delay(100);
@@ -86,44 +78,33 @@ void loop() {
 
 void report(double humidity, double tempC, double tempF, double heatIndexC, double heatIndexF) {
 
-  if (WiFi.status() == WL_CONNECTED)
-  {
+  StaticJsonBuffer<300> JSONbuffer;
+  JsonObject& JSONencoder = JSONbuffer.createObject();
 
-    StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
-    JsonObject& JSONencoder = JSONbuffer.createObject();
+  JSONencoder["humidity"] =  humidity;
+  JSONencoder["tempC"] = tempC;
+  JSONencoder["tempF"] = tempF;
+  JSONencoder["heatIndexC"] = heatIndexC;
+  JSONencoder["heatIndexF"] = heatIndexF;
 
+  char JSONmessageBuffer[300];
+  JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+  //Serial.println(JSONmessageBuffer);
 
-    JSONencoder["humidity"] =  humidity;
-    JSONencoder["tempC"] = tempC;
-    JSONencoder["tempF"] = tempF;
-    JSONencoder["heatIndexC"] = heatIndexC;
-    JSONencoder["heatIndexF"] = heatIndexF;
+  Serial.println("Authenticating Device...");
+  HTTPClient http;                                                                        //Declare object of class HTTPClient
+  http.begin("http://35.198.193.206/inputjson.php");                                                                                           //Specify request destination
+  http.addHeader("Content-Type", "application/json");                                     //Specify content-type header
+  http.addHeader("Accept", "application/json");
 
-    char JSONmessageBuffer[300];
-    JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-    //Serial.println(JSONmessageBuffer);
+  int httpCode = http.POST(JSONmessageBuffer);                                            //Send the request
+  String payload = http.getString();                                                      //Get the response payload
 
-    Serial.println("Authenticating Device...");
-    HTTPClient http;                                                                        //Declare object of class HTTPClient
-    http.begin("http://35.198.206.118/inputjson.php");                                                                                           //Specify request destination
-    http.addHeader("Content-Type", "application/json");                                     //Specify content-type header
-    http.addHeader("Accept", "application/json");
-
-    int httpCode = http.POST(JSONmessageBuffer);                                            //Send the request
-    String payload = http.getString();                                                      //Get the response payload
-
-    Serial.print("HTTPS CODE :");
-    Serial.println(httpCode);                                                              //Print HTTP return code
-    Serial.print("Payload :");
-    Serial.print(payload);                                                                 //Print request response payload
-    http.end();                                                                            //Close connection
-  }
-  else
-  {
-    Serial.println("Error in WiFi connection");
-  }
+  //Serial.print("HTTPS CODE :");
+  //Serial.println(httpCode);                                                              //Print HTTP return code
+  Serial.print("Payload :");
+  Serial.println(payload);                                                                 //Print request response payload
+  http.end();                                                                       //Send a request every 30 seconds
   Serial.println("Reported!");
-  delay(50000);                                                                          //Send a request every 30 seconds
-  
-
+  delay(5000);
 }
