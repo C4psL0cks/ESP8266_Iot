d026e6c925675b86b9af463aedb4c7a784b299f6
Esc <fenixalone@hotmail.com>
2018-11-12 05:40:56 +0700

    mz

--- ESP8266_HTTPS_GET_Requests.ino
+++ ESP8266_HTTPS_GET_Requests.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
  
-const char* ssid = "6021607";
-const char* password = "17401449";
+const char* ssid = "xxxxx";
+const char* password = "xxxxx";
  
 void setup () {
  
@@ -10,34 +10,22 @@ void setup () {
   WiFi.begin(ssid, password);
  
   while (WiFi.status() != WL_CONNECTED) {
- 
     delay(1000);
     Serial.print("Connecting..");
- 
-  }
- 
+  } 
 }
- 
 void loop() {
  
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
- 
     HTTPClient http;  //Declare an object of class HTTPClient
- 
     http.begin("http://jsonplaceholder.typicode.com/users/1");  //Specify request destination
     int httpCode = http.GET();                                                                  //Send the request
- 
     if (httpCode > 0) { //Check the returning code
- 
       String payload = http.getString();   //Get the request response payload
       Serial.println(payload);                     //Print the response payload
  
     }
- 
     http.end();   //Close connection
- 
   }
- 
   delay(30000);    //Send a request every 30 seconds
- 
 }
