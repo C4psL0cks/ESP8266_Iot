8aa13ac7abe3d2b98683275009649a3e7a81d587
C4psL0cks <fenixalone@hotmail.com>
2018-12-06 22:37:22 +0700

    mz

--- ESP8266_IO16PIN_MCP23017/ESP8266_IO16PIN_MCP23017.ino
+++ ESP8266_IO16PIN_MCP23017/ESP8266_IO16PIN_MCP23017.ino
@@ -2,27 +2,53 @@
 #include "Adafruit_MCP23017.h"
 Adafruit_MCP23017 mcp;
 
-int pin_led = 8; // IC ขา 8
-int pin_relay = 9;
-int pin_door = 10;
-int pin_buzzer = 12;
-int doorState = 0;
-int ledState = 0;
-#define closed 0
-#define opened 1
+// ซ้ายบนลงล่าง
+int pin_1 = 8;
+int pin_2 = 9;
+int pin_3 = 10;
+int pin_4 = 11;
+int pin_5 = 12;
+int pin_6 = 13;
+int pin_7 = 14;
+int pin_8 = 15;
+
+//ขวาบนลงล่าง
+int pin_28 = 7;
+int pin_27 = 6;
+int pin_26 = 5;
+int pin_25 = 4;
+int pin_24 = 3;
+int pin_23 = 2;
+int pin_22 = 1;
+int pin_21 = 0;
+
 
 void setup() {
   Serial.begin(115200);
-  mcp.begin(); // use default address 0
-  mcp.pinMode(pin_relay, OUTPUT);
+  mcp.begin(); 
+  mcp.pinMode(pin_1, OUTPUT);
+  mcp.pinMode(pin_2, OUTPUT);
+  mcp.pinMode(pin_3, OUTPUT);
+  mcp.pinMode(pin_4, OUTPUT);
+  mcp.pinMode(pin_5, OUTPUT);
+  mcp.pinMode(pin_6, OUTPUT);
+  mcp.pinMode(pin_7, OUTPUT);
+  mcp.pinMode(pin_8, OUTPUT);
+  mcp.pinMode(pin_28, OUTPUT);
+  mcp.pinMode(pin_27, OUTPUT);
+  mcp.pinMode(pin_26, OUTPUT);
+  mcp.pinMode(pin_25, OUTPUT);
+  mcp.pinMode(pin_24, OUTPUT);
+  mcp.pinMode(pin_23, OUTPUT);
+  mcp.pinMode(pin_22, OUTPUT);
+  mcp.pinMode(pin_21, OUTPUT);
 }
 
-// flip the pin #0 up and down
 void loop() {
-  mcp.digitalWrite(pin_led, HIGH);
-  mcp.digitalWrite(pin_relay, LOW);
+  mcp.digitalWrite(pin_1, HIGH);
+  mcp.digitalWrite(pin_2, LOW);
   delay(2000);
-  mcp.digitalWrite(pin_led, LOW);
-  mcp.digitalWrite(pin_relay, HIGH);
+  mcp.digitalWrite(pin_1, LOW);
+  mcp.digitalWrite(pin_2, HIGH);
   delay(2000);
 }
