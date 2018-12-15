2b919a9c2b2c913d535832850bbc235efc2d33f4
Esc <fenixalone@hotmail.com>
2018-12-16 03:58:18 +0700

    mz

--- ESP8266_WiFiManegerV1.ino
+++ ESP8266_WiFiManegerV1.ino
@@ -8,20 +8,14 @@
 #include <WiFiManager.h>
 #include <ArduinoJson.h>
 
-#include <TridentTD_LineNotify.h>
-
 WiFiManager wifiManager;
 
 char blynk_token[35] = "";
 char line_token[45] = "";
-char WIFISSID[20] = "";
-char WIFIPASS[20] = "";
-void Line_Notify(String message) ;
-int buttonStatelock, door = 1;
+
 //flag for saving data
 bool shouldSaveConfig = false;
 
-//callback notifying us of the need to sav config
 void saveConfigCallback () {
   Serial.println("Should save config");
   shouldSaveConfig = true;
@@ -31,8 +25,6 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println();
-  Serial.println(LINE.getVersion());
-  LINE.setToken(LINE_TOKEN);
 
   //clean FS, for testing
   SPIFFS.format();
@@ -41,17 +33,13 @@ void setup() {
   Serial.println("mounting FS...");
 
   if (SPIFFS.begin()) {
-    //Serial.println("mounted file system");
     if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
       Serial.println("reading config file");
       File configFile = SPIFFS.open("/config.json", "r");
       if (configFile) {
         Serial.println("opened config file");
         size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
         std::unique_ptr<char[]> buf(new char[size]);
-
         configFile.readBytes(buf.get(), size);
         DynamicJsonBuffer jsonBuffer;
         JsonObject& json = jsonBuffer.parseObject(buf.get());
@@ -70,79 +58,38 @@ void setup() {
   } else {
     Serial.println("failed to mount FS");
   }
-  //end read
 
-  // The extra parameters to be configured (can be either global or just in the setup)
-  // After connecting, parameter.getValue() will get you the configured value
-  // id/name placeholder/prompt default length
 
-  WiFiManagerParameter custom_blynk_token("BLYNK", "blynk token", blynk_token, 35);
-  WiFiManagerParameter custom_line_token("LINE", "line token", line_token, 45);
 
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
-  //WiFiManager wifiManager;
-
-  //set config save notify callback
+  wifiManager.resetSettings();
   wifiManager.setSaveConfigCallback(saveConfigCallback);
 
-  //set static ip
-  //wifiManager.setSTAStaticIPConfig(IPAddress(192, 168, 1, 99), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
-
-  //add all your parameters here
-
+  WiFiManagerParameter custom_blynk_token("BLYNK", "blynk token", blynk_token, 35);
   wifiManager.addParameter(&custom_blynk_token);
+  WiFiManagerParameter custom_line_token("LINE", "line token", line_token, 45);
   wifiManager.addParameter(&custom_line_token);
 
-  //reset settings - for testing
-  wifiManager.resetSettings();
 
-
-  //set minimu quality of signal so it ignores AP's under that quality
-  //defaults to 8%
-  wifiManager.setMinimumSignalQuality();
-
-  //sets timeout until configuration portal gets turned off
-  //useful to make it all retry or go to sleep
-  //in seconds
   //wifiManager.setTimeout(120);
 
-  //fetches ssid and pass and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
   if (!wifiManager.autoConnect("TEST", "password")) {
     Serial.println("failed to connect and hit timeout");
     delay(3000);
-    //reset and try again, or maybe put it to deep sleep
-
     ESP.reset();
     delay(5000);
   }
-  wifiManager.getSSID().toCharArray(WIFISSID, 20);
-  wifiManager.getPassword().toCharArray(WIFIPASS, 20);
-  //  Serial.println("******************************");
-  //  Serial.println(WIFISSID);
-  //  Serial.println(WIFIPASS);
-  //  Serial.println("******************************");
-
-  //if you get here you have connected to the WiFi
+  
   Serial.println("connected...sucessfull:)");
-
-
-  //read updated parameters
-
-  strcpy(blynk_token, custom_blynk_token.getValue());
-  strcpy(line_token, custom_line_token.getValue());
 
   //save the custom parameters to FS
   if (shouldSaveConfig) {
     Serial.println("saving config");
     DynamicJsonBuffer jsonBuffer;
     JsonObject& json = jsonBuffer.createObject();
+    
+    strcpy(blynk_token, custom_blynk_token.getValue());
+    strcpy(line_token, custom_line_token.getValue());
 
-    json["WIFISSID"] = WIFISSID;
-    json["WIFIPASS"] = WIFIPASS;
     json["blynk_token"] = blynk_token;
     json["line_token"] = line_token;
 
@@ -159,25 +106,10 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  Blynk.begin(blynk_token,"blynk-cloud.com", 8442);
 
-  Serial.println(blynk_token);
-  Blynk.begin(blynk_token, WIFISSID, WIFIPASS, "blynk-cloud.com", 8442);
-
-}
-BLYNK_WRITE(V1) {
-  buttonStatelock = param.asInt();
-  if (buttonStatelock == 0) {
-   LINE.notify(" 1 : OFF");
-  }
-  else if (buttonStatelock == 1) {
-    LINE.notify(" 1 : ON");
-  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Blynk.run();
-  Blynk.virtualWrite(10, buttonStatelock);
-  Blynk.virtualWrite(11, door);
 
 }
