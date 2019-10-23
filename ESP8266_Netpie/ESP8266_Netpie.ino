fb9a9297cb182507e810fc0f44223d19f581b81b
Mz'Makerz <fenixalone@hotmail.com>
2019-10-23 17:53:31 +0700

    update by mz

--- ESP8266_Netpie/ESP8266_Netpie.ino
+++ ESP8266_Netpie/ESP8266_Netpie.ino
@@ -3,9 +3,10 @@
 #include <ArduinoJson.h>
 #include <TridentTD_LineNotify.h>
 #include <Wire.h>
+#include <time.h>
 #include <ACROBOTIC_SSD1306.h>
-const char* ssid     = "6021607";
-const char* password = "17401449";
+#define SSID_WIFI    "AndroidAPD261"
+#define PASSWORD_WIFI   "mojijosh"
 #define LINE_TOKEN  "0lbZcis2QugRyJv6SZaxzcK2STZOkzWqD7TnDqegFbM"
 #define APPID "smartbins"
 #define KEY "4gb6nh27xR6cc6W"
@@ -29,8 +30,16 @@ char str[32];
 long duration, distance;
 float temp;
 int Read;
-int Val;
+float Val;
 
+/// time
+struct tm* p_tm;
+char ntp_server1[20] = "ntp.ku.ac.th";
+char ntp_server2[20] = "fw.eng.ku.ac.th";
+char ntp_server3[20] = "time.uni.net.th";
+int sec;
+String times = "";
+bool checksend = false;
 
 MicroGear microgear(client);
 void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
@@ -52,73 +61,79 @@ void setup() {
   Serial.println("Starting...");
   oled.setTextXY(1, 0);             // Set cursor position, start of line 1
   oled.putString("Starting...");
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID_WIFI, PASSWORD_WIFI);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    oled.setTextXY(2, 0);             // Set cursor position, start of line 2
-    oled.putString(".");
   }
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   LINE.setToken(LINE_TOKEN);
+  configTime(7 * 3600, 0, ntp_server1, ntp_server2, ntp_server3);
+  Serial.println("\nWaiting for time");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
+  }
   LINE.notify("เชื่อมต่อเรียบร้อย....");
-  oled.setTextXY(2, 0);             // Set cursor position, start of line 2
-  oled.putString("WiFi connected");
-  oled.setTextXY(3, 0);             // Set cursor position, start of line 3
-  oled.putString("IP address: ");
-  oled.setTextXY(4, 0);             // Set cursor position, start of line 4
-  oled.putString(WiFi.localIP().toString());
-  //delay(1500);
-  oled.clearDisplay();              // Clear screen
-
+  oled.clearDisplay();
   // netpie
   microgear.init(KEY, SECRET, ALIAS);
   microgear.connect(APPID);
 }
 void loop() {
 
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-
-  Read = analogRead(A0);
-  temp = Read / 4.092;
-  Read = (int)temp;
-  Val = ((Read % 100) / 10.0);
-  Serial.println("Voltage:" + String(Val));
-  oled.setTextXY(2, 0);
-  oled.putString("Voltage:" + String(Val));
-
-  pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(TRIGGER_PIN, LOW);
-  //delayMicroseconds(2);
-  digitalWrite(TRIGGER_PIN, HIGH);
-  //delayMicroseconds(5);
-  digitalWrite(TRIGGER_PIN, LOW);
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distance = (duration / 2) / 29.1;
-  Serial.print(distance);
-  oled.setTextXY(3, 0);             
-  oled.putString("Distance:" + String(distance));
-  //Serial.println(" cm");
-  //  if (distance <= 15) {
-  //    LINE.notify("ถังขยะเต็ม");
-  //  }
-  root["bin"] = distance;
-  root["voltage"] = Val;
-  String jsonData;
-  root.printTo(jsonData);
-  //Serial.println(jsonData);
-
   if (microgear.connected()) {
     Serial.println("connected");
     microgear.loop();
 
+    time_t time_now = time(nullptr);   // Time
+    p_tm = localtime(&time_now);
+    sec = p_tm->tm_min; //tm_min
+    times = ctime(&time_now);
+
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+
+    Read = analogRead(A0);
+    temp = Read / 5.092;
+    Read = (int)temp;
+    Val = ((Read % 100) / 10.0);
+    Serial.println("Voltage:" + String(Val));
+
+    pinMode(TRIGGER_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    pinMode(BUILTIN_LED, OUTPUT);
+    digitalWrite(TRIGGER_PIN, LOW);
+    //delayMicroseconds(2);
+    digitalWrite(TRIGGER_PIN, HIGH);
+    //delayMicroseconds(5);
+    digitalWrite(TRIGGER_PIN, LOW);
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = (duration / 2) / 29.1;
+    //Serial.println(distance);
+    
+    if ((sec % 5) == 0) {
+      Serial.println("CHECK:" + String(sec));
+      if (distance < 10  && checksend == false) {
+        LINE.notify("ถังขยะเต็ม");
+        checksend = true;
+      }
+    }
+    else {
+      Serial.println("RESRT:" + String(sec));
+      checksend = false;
+    }
+
+    root["bin"] = distance;
+    root["voltage"] = Val;
+    String jsonData;
+    root.printTo(jsonData);
+    //Serial.println(jsonData);
+
     if (timer >= 1000) {
-      sprintf(str, "%d,%d", distance, Val);
+      sprintf(str, "%d,%.2f", distance, Val);
       Serial.println(str);
       Serial.print("Sending -- >");
       microgear.publish("/sensor", str);
