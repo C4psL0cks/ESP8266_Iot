059ddc7e157222296647f23405d72311f7d40c7f
Mz'Makerz <fenixalone@hotmail.com>
2019-10-24 14:05:04 +0700

    update by mz

--- ESP8266_Netpie/ESP8266_Netpie.ino
+++ ESP8266_Netpie/ESP8266_Netpie.ino
@@ -1,13 +1,20 @@
 #include <ESP8266WiFi.h>
+// Netpie
 #include <MicroGear.h>
+// JSON
 #include <ArduinoJson.h>
+// LINE
 #include <TridentTD_LineNotify.h>
 #include <Wire.h>
 #include <time.h>
+// OLED
 #include <ACROBOTIC_SSD1306.h>
+// wifi
 #define SSID_WIFI    "AndroidAPD261"
 #define PASSWORD_WIFI   "mojijosh"
+// Line
 #define LINE_TOKEN  "0lbZcis2QugRyJv6SZaxzcK2STZOkzWqD7TnDqegFbM"
+// Netpie
 #define APPID "smartbins"
 #define KEY "4gb6nh27xR6cc6W"
 #define SECRET "6QIrz3dXFOBNbp4P3hVX6SulZ"
@@ -19,12 +26,14 @@
 #define BAUD_RATE 115200
 #define MAX_TEMP 100
 #define MAX_HUMID 100
-// sensor
+
+// sensor Ultrasonic
 #define TRIGGER_PIN  D5
 #define ECHO_PIN     D6
 
 WiFiClient client;
 
+// ตัวแปลสำหรับ sensor ทั้งสองตัว
 int timer = 0;
 char str[32];
 long duration, distance;
@@ -32,13 +41,14 @@ float temp;
 int Read;
 float Val;
 
-/// time
+/// time NTP server
 struct tm* p_tm;
 char ntp_server1[20] = "ntp.ku.ac.th";
 char ntp_server2[20] = "fw.eng.ku.ac.th";
 char ntp_server3[20] = "time.uni.net.th";
 int sec;
 String times = "";
+// check รอบการส่งไลน์
 bool checksend = false;
 
 MicroGear microgear(client);
@@ -61,6 +71,7 @@ void setup() {
   Serial.println("Starting...");
   oled.setTextXY(1, 0);             // Set cursor position, start of line 1
   oled.putString("Starting...");
+  // ส่วนของการเชื่อม wifi
   WiFi.begin(SSID_WIFI, PASSWORD_WIFI);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -69,7 +80,9 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  // เชื่อมต่อ line notify
   LINE.setToken(LINE_TOKEN);
+  // ส่วนของการเชื่อมต่อ NTP server
   configTime(7 * 3600, 0, ntp_server1, ntp_server2, ntp_server3);
   Serial.println("\nWaiting for time");
   while (!time(nullptr)) {
@@ -77,6 +90,7 @@ void setup() {
     delay(1000);
   }
   LINE.notify("เชื่อมต่อเรียบร้อย....");
+  // clear  จอ
   oled.clearDisplay();
   // netpie
   microgear.init(KEY, SECRET, ALIAS);
@@ -89,19 +103,23 @@ void loop() {
     microgear.loop();
 
     time_t time_now = time(nullptr);   // Time
-    p_tm = localtime(&time_now);
-    sec = p_tm->tm_min; //tm_min
-    times = ctime(&time_now);
+    p_tm = localtime(&time_now);  // ตัวแปล time
+    sec = p_tm->tm_min; //tm_min // เวลาสปัจุบัญที่เป็นหน่วย นาที เอาใช้สำหรับเช็คการส่ง line
+    times = ctime(&time_now);  // เวลาปัจุบัญที่ได้มาจาก NTP server
 
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& root = jsonBuffer.createObject();
+    DynamicJsonBuffer jsonBuffer;   // กำหนด jsonbuffer แบบ Dynamic(ไม่กำหนดตายตัว ขยายได้เรือยๆ)
+    JsonObject& root = jsonBuffer.createObject(); // สร้าง object มารอรับค่าที่ชื่อว่า root[]
 
-    Read = analogRead(A0);
+    // ส่วนของ sensor voltage
+    Read = analogRead(A0); // อ่านค่าจาก senoser มาที่ขา A0 แบบ Analog
     temp = Read / 5.092;
     Read = (int)temp;
-    Val = ((Read % 100) / 10.0);
+    Val = ((Read % 100) / 10.0);  // ค่าที่ได้จากการคำนวนของ sensor
     //Serial.println("Voltage:" + String(Val));
+    oled.setTextXY(2, 0);             // Set cursor position, start of line 1
+    oled.putString("Volt:" + String(Val));
 
+    // ส่วนของ sensor Ultrasonic
     pinMode(TRIGGER_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
     digitalWrite(TRIGGER_PIN, LOW);
@@ -112,13 +130,15 @@ void loop() {
 
     duration = pulseIn(ECHO_PIN, HIGH);
     distance = duration * 0.034 / 2;
-    if (distance > 30) {
-      distance = 0;
+    if (distance > 30) { // ปกติถังลึก 30 cm ถ้า sensor อ่านได้ เกิน 30 แสดงว่าเกิดค่าที่ error ให้ set ค่ากลับไปเป็น 0
+      distance = 0; // set ให้ = 0
     }
     //Serial.println(distance);
+    oled.setTextXY(3, 0);             // Set cursor position, start of line 1
+    oled.putString("CM:" + String(distance));
 
     if ((sec % 5) == 0) {
-      Serial.println("CHECK:" + String(sec)); 
+      Serial.println("CHECK:" + String(sec));
       // ถ้า sec ก็คือ หน่วยนาที มันหาร 5 ลงตัว มันจะเข้าเคสนี้ละเช็คต่อว่า ขยะ เต็มมั้ยถ้าเต็ม มันจะส่งไปที่ไลน์ แค่ครั่งเดียว
       if (distance < 10 && checksend == false) { // เช็คว่า ขยะเต็มมั้ยถ้าเต็ม
         LINE.notify("ถังขยะเต็ม"); // ส่งไปที่ line
@@ -131,18 +151,18 @@ void loop() {
       checksend = false;
     }
 
-    root["bin"] = distance;
-    root["voltage"] = Val;
+    root["bin"] = distance;  // เอาค่าตัวแปลของ ถังขยะ มาเก็บใส่ json
+    root["voltage"] = Val;  // เอาค่าตัวแปลของ แบต มาเก็บใส่ json
     String jsonData;
-    root.printTo(jsonData);
+    root.printTo(jsonData); // จับยัดเข้า object json
     //Serial.println(jsonData);
 
-    if (timer >= 1000) {
-      sprintf(str, "%d,%.2f", distance, Val);
+    if (timer >= 1000) { // ทุกๆ 1 วิจะทำการส่งค่าไป netpie
+      sprintf(str, "%d,%.2f", distance, Val);  // แสดงค่าที่ได้รับมาจากด้านบน
       Serial.println(str);
       Serial.print("Sending -- >");
-      microgear.publish("/sensor", str);
-      microgear.writeFeed(FEEDID, jsonData);
+      microgear.publish("/sensor", str);  // ส่งค่าไปยัง netpie ที่ path/sensor
+      microgear.writeFeed(FEEDID, jsonData); // ส่งไปแบบ json
       timer = 0;
     } else {
       timer += 100;
